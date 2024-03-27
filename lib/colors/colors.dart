a7972c086d5a369441e1d96b74ec93eca4897271
sanjaytamilan0 <164481795+sanjaytamilan0@users.noreply.github.com>
2024-03-27 17:06:10 +0530

    Delete lib directory

--- lib/colors/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-import 'package:flutter/material.dart';
-
-class MyColors {
-   static const Color appColor = Colors.green;
-   static Color appbarColor = Color(0xFFC4DFE6);
-}
--- lib/ui/userscreen/firestore/user_firestore/product_cubit/product_state.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_store/ui/userscreen/firestore/user_firestore/product_cubit/productsmodel.dart';
-
-class ProductState {
-  final List<Product> products;
-
-  ProductState(this.products);
-}
